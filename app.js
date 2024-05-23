fad4e9393d6a40ed28630e59411a8fdc329eb43f
alisoomro786 <soomroali0698>
2024-05-23 10:25:48 +0500

    work done

---
+++ app.js
@@ -0,0 +1,48 @@
+//data types
+// "hello world" string
+//1,2,232  integer
+//1.2 3.4 float
+ //var qwe ='hi hello sindh'
+ //alert (qwe)
+//document.write ('<h1>helloworld</h1>')
+////document.write('<h1>'+'helloworld'+'</h1>')
+//var a ='alisoomro'
+//document.write(a)
+//var start use only $ _ also alphate wx var _abc,abc,$abc
+//var firstName ='ali '
+//var lastName ='soomro'
+//document.write(firstName +""+ lastName)
+//var num1 ='44'
+//document.write (num1)
+//var num1=10
+//var num2=20
+//document.write(num1+num2)
+  //     =30
+
+  //var num1='10'
+//var num2='20'
+//document.write(num1+num2)
+     //  =1020
+//        var num1='10'
+// var num2=20
+// document.write(num1+num2)
+// //          =1020
+// var num1='10'
+// var num2=20
+// document.write(num1-num2)
+// //            = -10
+//            var num1='10'
+//            var num2=20
+//            document.write(num1*num2)
+//                   =200
+// + k sign pe concat kargega us surat pe jab string hoge
+// + k sing pe add karega jab dono integerh hoon
+// - sub karega jab 1 string or 1 integer ho tab bhi or jab dono int hotb bhi
+// same minus wali cheez mul div mod pe apply hogi
+// jab ek string ho usmain 'alphabe to ' or ek int ho or min mul div laga h hoto to runtime 
+// pe decide karega or operation perfom kardega
+// var a= prompt ('enter value 1')
+// document.write(a)
+var a=prompt("enter value 1")
+var b= prompt("enter value 2")
+document.write(a*b)
